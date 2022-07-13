9975b1f312ed5bc0a3c6348fd50ad78e6e687612
David Veríssimo Leite <103391404+davidvleite@users.noreply.github.com>
2022-07-13 16:47:48 -0300

    Commit1

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using tabuleiro;
+namespace xadrez_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posicao: " + P);
+            Console.ReadLine();
+        }
+    }
+}
